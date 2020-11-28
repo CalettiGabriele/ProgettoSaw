24e7990504d261537a964246327ada2e111f6905
gabriele caletti <gabrielecaletti@MBP-di-gabriele.fritz.box>
2020-11-28 10:02:03 +0100

    aggiunto commenti e corretto indentazione

--- Home.php
+++ Home.php
@@ -4,7 +4,7 @@
 
     <title>Home</title>
 
-    <!-- bootstrap -->
+<!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
@@ -12,16 +12,17 @@
 </head>
 <body>
 
-<?php
-  session_start();
+<!-- inserimento navbar -->
+    <?php
+    session_start();
 
-  if(isset($_SESSION["login"])) {
-    include 'NavbarAutenticato.php';
-  }
-  else{
-    include 'NavbarNonAutenticato.php';
-  }
-?>
+    if(isset($_SESSION["login"])) {
+        include 'NavbarAutenticato.php';
+    }
+    else{
+        include 'NavbarNonAutenticato.php';
+    }
+    ?>
 
 </body>
 </html>
